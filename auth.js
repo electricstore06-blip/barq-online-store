4d0a65e0352bad4b8039063fc4e7585205b5058e
electricstore06-blip <electricstore06@gmail.com>
2026-09-12 02:04:15 +0300

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,27 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { 
+  getAuth, 
+  createUserWithEmailAndPassword, 
+  signInWithEmailAndPassword,
+  signOut
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+import { firebaseConfig } from "./firebase-config.js";
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+// Register new user
+export function register(email, password) {
+  return createUserWithEmailAndPassword(auth, email, password);
+}
+
+// Login user
+export function login(email, password) {
+  return signInWithEmailAndPassword(auth, email, password);
+}
+
+// Logout user
+export function logout() {
+  return signOut(auth);
+}
