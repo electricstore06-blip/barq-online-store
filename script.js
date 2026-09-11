8cc1f788c8c12d6b38b1a24b39fb5d4d69f4bf13
electricstore06-blip <electricstore06@gmail.com>
2026-09-11 06:02:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,56 +1,99 @@
+let cart = [];
+
+
+function addToCart(name, price){
+
+    let existing = cart.find(item => item.name === name);
+
+
+    if(existing){
+
+        existing.quantity++;
+
+    } else {
+
+        cart.push({
+            name: name,
+            price: price,
+            quantity: 1
+        });
+
+    }
+
+
+    updateCart();
+
+}
+
+
+
 function updateCart(){
 
     let cartBox = document.getElementById("cartItems");
     let totalBox = document.getElementById("cartTotal");
+
 
     cartBox.innerHTML = "";
 
     let total = 0;
 
 
-    cart.forEach((item,index)=>{
+    cart.forEach(function(item,index){
 
         total += item.price * item.quantity;
 
 
-        cartBox.innerHTML += `
-        
-        <div class="cart-product">
+        cartBox.innerHTML +=
+        "<div class='cart-product'>" +
 
-            <div>
-                <strong>${item.name}</strong>
-                <br>
-                ${item.price} ريال x ${item.quantity}
-            </div>
+        "<div>" +
+        "<strong>" + item.name + "</strong><br>" +
+        item.price + " ريال x " + item.quantity +
+        "</div>" +
 
+        "<div>" +
 
-            <div>
+        "<button onclick='changeQuantity("+index+",-1)'>−</button>" +
 
-                <button onclick="changeQuantity(${index},-1)">
-                −
-                </button>
+        "<button onclick='changeQuantity("+index+",1)'>+</button>" +
 
-                <span>${item.quantity}</span>
+        "<button onclick='removeItem("+index+")'>✕</button>" +
 
-                <button onclick="changeQuantity(${index},1)">
-                +
-                </button>
+        "</div>" +
 
-
-                <button onclick="removeItem(${index})">
-                ✕
-                </button>
-
-            </div>
-
-        </div>
-
-        `;
-
+        "</div>";
 
     });
 
 
     totalBox.innerHTML = "Total: " + total.toFixed(2) + " ريال";
+
+}
+
+
+
+function changeQuantity(index,value){
+
+    cart[index].quantity += value;
+
+
+    if(cart[index].quantity <= 0){
+
+        cart.splice(index,1);
+
+    }
+
+
+    updateCart();
+
+}
+
+
+
+function removeItem(index){
+
+    cart.splice(index,1);
+
+    updateCart();
 
 }
