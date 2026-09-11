29acc0c10c578690041efe469731063e41e45d35
electricstore06-blip <electricstore06@gmail.com>
2026-09-12 01:42:59 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCV3N68e7GXNgNALXTiWjsYOQVFhqisTIc",
+  authDomain: "barq-online-store.firebaseapp.com",
+  projectId: "barq-online-store",
+  storageBucket: "barq-online-store.firebasestorage.app",
+  messagingSenderId: "44237208872",
+  appId: "1:44237208872:web:525a5bcf8c7b9d5978e41d",
+  measurementId: "G-KS2BC3EXDD"
+};
+export { firebaseConfig };
