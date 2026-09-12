f08b5c99a93123032a7150caaecd4df2deac98ac
electricstore06-blip <electricstore06@gmail.com>
2026-09-12 04:16:58 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,10 @@
-export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCV3N68e7GXNgNALXTiWjsYOQVFhqisTIc",
+  authDomain: "barq-online-store.firebaseapp.com",
+  projectId: "barq-online-store",
+  storageBucket: "barq-online-store.firebasestorage.app",
+  messagingSenderId: "44237208872",
+  appId: "1:44237208872:web:525a5bcf8c7b9d5978e41d",
+  measurementId: "G-KS2BC3EXDD"
 };
