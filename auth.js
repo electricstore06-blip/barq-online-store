9625017289c74f51aa5174831f7e2f170eabb13f
electricstore06-blip <electricstore06@gmail.com>
2026-09-12 12:03:28 +0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,6 +1,7 @@
 import { initializeApp } from 
 "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
+
 import {
 getAuth,
 createUserWithEmailAndPassword,
@@ -15,11 +16,13 @@ from
 import { firebaseConfig } from "./firebase-config.js";
 
 
-// Firebase start
+
+// FIREBASE START
 
 const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
+
 
 
 
@@ -33,6 +36,7 @@ document.getElementById("loginBox").style.display="block";
 
 
 
+
 // CLOSE LOGIN
 
 window.closeLogin=function(){
@@ -43,33 +47,57 @@ document.getElementById("loginBox").style.display="none";
 
 
 
+
+
 // REGISTER
 
 window.registerUser=function(){
+
 
 let email=document.getElementById("email").value;
 
 let password=document.getElementById("password").value;
 
 
+
+if(email==="" || password===""){
+
+alert("Please enter email and password");
+
+return;
+
+}
+
+
+
 createUserWithEmailAndPassword(auth,email,password)
+
 
 .then(()=>{
 
+
 alert("Account created successfully");
+
 
 closeLogin();
 
+
 })
+
 
 .catch(error=>{
 
+
 alert(error.message);
+
 
 });
 
 
 };
+
+
+
 
 
 
@@ -77,29 +105,54 @@ alert(error.message);
 
 window.loginUser=function(){
 
+
+
 let email=document.getElementById("email").value;
 
 let password=document.getElementById("password").value;
 
 
+
+if(email==="" || password===""){
+
+
+alert("Please enter email and password");
+
+
+return;
+
+
+}
+
+
+
 signInWithEmailAndPassword(auth,email,password)
+
 
 .then(()=>{
 
+
 alert("Login successful");
+
 
 closeLogin();
 
+
 })
+
 
 .catch(error=>{
 
+
 alert(error.message);
+
 
 });
 
 
 };
+
+
 
 
 
@@ -109,15 +162,22 @@ alert(error.message);
 
 window.logoutUser=function(){
 
+
+
 signOut(auth)
+
 
 .then(()=>{
 
+
 alert("Logged out");
+
 
 location.reload();
 
+
 });
+
 
 };
 
@@ -125,12 +185,22 @@ location.reload();
 
 
 
-// CHECK LOGIN STATUS
+
+
+
+// CHECK USER STATUS
 
 onAuthStateChanged(auth,(user)=>{
 
 
-const loginButton = document.getElementById("loginButton");
+
+const loginButton =
+document.getElementById("loginButton");
+
+const adminLink =
+document.getElementById("adminLink");
+
+
 
 
 if(!loginButton){
@@ -141,26 +211,55 @@ return;
 
 
 
+
 if(user){
 
 
-loginButton.innerHTML="👤 "+user.email+" | Logout";
+
+loginButton.innerHTML =
+"👤 "+user.email+" | Logout";
 
 
-loginButton.onclick = logoutUser;
+loginButton.onclick =
+logoutUser;
+
+
+
+
+// SHOW ADMIN FOR ADMIN EMAIL
+
+if(adminLink){
+
+
+adminLink.style.display="block";
+
+
+}
 
 
 
 }else{
 
 
+
 loginButton.innerHTML="🔒 Login";
 
 
-loginButton.onclick = showLogin;
+loginButton.onclick=showLogin;
+
+
+
+if(adminLink){
+
+
+adminLink.style.display="none";
 
 
 }
+
+
+}
+
 
 
 });
