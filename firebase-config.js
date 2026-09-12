da0d0da2e83eefb23f378267789153026cd093a3
electricstore06-blip <electricstore06@gmail.com>
2026-09-12 11:14:20 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,20 @@
-// Firebase configuration
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-export const firebaseConfig = {
-  apiKey: "AIzaSyCV3N68e7GXNgNALXTiWjsYOQVFhqisTIc",
-  authDomain: "barq-online-store.firebaseapp.com",
+
+const firebaseConfig = {
+
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
   projectId: "barq-online-store",
-  storageBucket: "barq-online-store.firebasestorage.app",
-  messagingSenderId: "44237208872",
-  appId: "1:44237208872:web:525a5bcf8c7b9d5978e41d",
-  measurementId: "G-KS2BC3EXDD"
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_ID",
+  appId: "YOUR_APP_ID"
+
 };
+
+
+const app = initializeApp(firebaseConfig);
+
+
+export const db = getFirestore(app);
