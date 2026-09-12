1740b8dd6dc7a893854eb567ccd42b13a8f2ba75
electricstore06-blip <electricstore06@gmail.com>
2026-09-12 12:20:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,21 @@
-import { db } from "./firebase-config.js";
-
-import {
-collection,
-getDocs
-}
-from
-"https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
-
-
-// CART
-
-let cart = [];
-
-
-
-
-// LOAD PRODUCTS
-
 async function loadProducts(){
 
-try{
+try {
 
+const querySnapshot = await getDocs(collection(db,"products"));
 
-const querySnapshot = await getDocs(
-collection(db,"products")
-);
-
-
-let products=[];
+let products = [];
 
 
 querySnapshot.forEach((doc)=>{
 
-
 products.push({
 
-id:doc.id,
+id: doc.id,
 
 ...doc.data()
 
 });
-
 
 });
 
@@ -52,7 +26,6 @@ console.log(products);
 displayProducts(products);
 
 
-
 }
 
 catch(error){
@@ -61,256 +34,4 @@ console.log("Firebase Error:",error);
 
 }
 
-
 }
-
-
-
-
-// DISPLAY PRODUCTS
-
-
-function displayProducts(products){
-
-
-let container=document.getElementById("products");
-
-
-if(!container){
-
-return;
-
-}
-
-
-
-container.innerHTML="";
-
-
-
-products.forEach(product=>{
-
-
-container.innerHTML += `
-
-
-<div class="product">
-
-
-<img src="${product.image}">
-
-
-<h3>${product.name}</h3>
-
-
-<p>${product.brand}</p>
-
-
-<p class="price">${product.price} ريال</p>
-
-
-<button onclick="addToCart('${product.name}',${product.price})">
-
-Add To Cart
-
-</button>
-
-
-</div>
-
-
-`;
-
-
-});
-
-
-}
-
-
-
-
-
-
-// ADD CART
-
-
-function addToCart(name,price){
-
-
-cart.push({
-
-name:name,
-
-price:price
-
-});
-
-
-document.getElementById("cartCount").innerHTML=cart.length;
-
-
-updateCart();
-
-
-}
-
-
-
-
-
-// OPEN CART
-
-function openCart(){
-
-
-document.getElementById("cartBox").style.display="block";
-
-
-updateCart();
-
-
-}
-
-
-
-
-// CLOSE CART
-
-function closeCart(){
-
-
-document.getElementById("cartBox").style.display="none";
-
-
-}
-
-
-
-
-
-
-// UPDATE CART
-
-
-function updateCart(){
-
-
-let items=document.getElementById("cartItems");
-
-let total=document.getElementById("cartTotal");
-
-
-if(!items || !total){
-
-return;
-
-}
-
-
-
-items.innerHTML="";
-
-
-let sum=0;
-
-
-
-cart.forEach((item,index)=>{
-
-
-items.innerHTML += `
-
-
-<p>
-
-${item.name}
-
-<br>
-
-${item.price} ريال
-
-
-<button onclick="removeCart(${index})">
-
-X
-
-</button>
-
-
-</p>
-
-<hr>
-
-
-`;
-
-
-sum += item.price;
-
-
-});
-
-
-total.innerHTML=sum;
-
-
-}
-
-
-
-
-
-
-// REMOVE
-
-
-function removeCart(index){
-
-
-cart.splice(index,1);
-
-
-document.getElementById("cartCount").innerHTML=cart.length;
-
-
-updateCart();
-
-
-}
-
-
-
-
-
-
-// CHECKOUT
-
-
-function checkout(){
-
-
-alert("Thank you for shopping with BARQ!");
-
-}
-
-
-
-
-
-// GLOBAL FUNCTIONS
-
-
-window.addToCart=addToCart;
-
-window.openCart=openCart;
-
-window.closeCart=closeCart;
-
-window.removeCart=removeCart;
-
-window.checkout=checkout;
-
-
-
-loadProducts();
