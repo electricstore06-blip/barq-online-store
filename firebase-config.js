cfb6ea3fb38be34c3b0dc4aa5dbec634a5a72e64
electricstore06-blip <electricstore06@gmail.com>
2026-09-12 04:12:47 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,8 @@
 export const firebaseConfig = {
-
-  apiKey: "PUT_YOUR_API_KEY_HERE",
-
-  authDomain: "PUT_YOUR_AUTH_DOMAIN_HERE",
-
-  projectId: "PUT_YOUR_PROJECT_ID_HERE",
-
-  storageBucket: "PUT_YOUR_STORAGE_BUCKET_HERE",
-
-  messagingSenderId: "PUT_YOUR_SENDER_ID_HERE",
-
-  appId: "PUT_YOUR_APP_ID_HERE"
-
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
 };
