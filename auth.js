26c908bb742be38e55b9f5f9f0ff822b6ad30b84
electricstore06-blip <electricstore06@gmail.com>
2026-09-12 03:30:14 +0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,156 +1,92 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { initializeApp } from 
+"https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
 import {
 getAuth,
 createUserWithEmailAndPassword,
 signInWithEmailAndPassword,
 signOut
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+}
+from
+"https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 
 import { firebaseConfig } from "./firebase-config.js";
 
-
-// Firebase start
 
 const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
 
 
-// Open Login Box
 
-function showLogin(){
+window.registerUser = function(){
 
-document.getElementById("loginBox").style.display="block";
-
-}
+let email = document.getElementById("email").value;
+let password = document.getElementById("password").value;
 
 
-// Close Login Box
+createUserWithEmailAndPassword(auth,email,password)
 
-function closeLogin(){
-
-document.getElementById("loginBox").style.display="none";
-
-}
-
-
-
-// Register
-
-async function registerUser(){
-
-let email=document.getElementById("email").value;
-
-let password=document.getElementById("password").value;
-
-
-try{
-
-await createUserWithEmailAndPassword(
-auth,
-email,
-password
-);
-
+.then(()=>{
 
 alert("Account created successfully");
 
+})
 
-closeLogin();
-
-
-}
-
-catch(error){
+.catch((error)=>{
 
 alert(error.message);
 
-}
-
-}
+});
 
 
-
-// Login
-
-async function loginUser(){
-
-let email=document.getElementById("email").value;
-
-let password=document.getElementById("password").value;
+};
 
 
-try{
 
 
-await signInWithEmailAndPassword(
-auth,
-email,
-password
-);
 
+window.loginUser = function(){
+
+let email = document.getElementById("email").value;
+let password = document.getElementById("password").value;
+
+
+signInWithEmailAndPassword(auth,email,password)
+
+.then(()=>{
 
 alert("Login successful");
 
-
-closeLogin();
-
-
-}
+document.getElementById("loginBox").style.display="none";
 
 
-catch(error){
+})
+
+.catch((error)=>{
 
 alert(error.message);
 
-}
+});
 
 
-}
+};
 
 
 
-// Logout
 
-async function logoutUser(){
 
-await signOut(auth);
+window.logoutUser=function(){
+
+
+signOut(auth)
+
+.then(()=>{
 
 alert("Logged out");
 
-}
-
-
-
-// Make functions available to HTML
-
-window.showLogin = showLogin;
-
-window.closeLogin = closeLogin;
-
-window.registerUser = registerUser;
-
-window.loginUser = loginUser;
-
-window.logoutUser = logoutUser;
-
-
-
-// Button listener
-
-document.addEventListener("DOMContentLoaded",()=>{
-
-
-let btn=document.getElementById("loginBtn");
-
-
-if(btn){
-
-btn.addEventListener("click",showLogin);
-
-}
-
-
 });
+
+
+};
